55a22d11a82c9496aa64c437cf2a86948b63c6a6
Henry Allen <Heman7557@gmail.com>
2022-02-15 15:35:58 -0800

    fix

--- src/main/java/Weed.java
+++ src/main/java/Weed.java
@@ -8,32 +8,4 @@ public class Weed extends Drugs {
         super(price, lean, amount);
     }
 
-    public void updateWeed() {
-        super.updateDrug();
-    }
-
-    public int getAmount() {
-        return super.getAmount();
-    }
-
-    public void setAmount(int amount) {
-        super.setAmount(amount);
-    }
-
-    public int getPrice() {
-        return super.getPrice();
-    }
-
-    public double getLean() {
-        return super.getLean();
-    }
-
-    public void setPrice(int price) {
-        super.setPrice(price);
-    }
-
-    public void setLean(double lean) {
-        super.setLean(lean);
-    }
-
 }
